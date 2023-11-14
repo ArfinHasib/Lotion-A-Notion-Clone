cad472f017fa8f2931c081a2d5f9e3896cfb28dc
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-14 21:46:44 +0600

    Building the sidebar

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
    providers: [
       {
-         domain: 'https://tight-sturgeon-99.clerk.accounts.dev',
+         domain: 'https://moved-bull-64.clerk.accounts.dev',
          applicationID: 'convex',
       },
    ],
