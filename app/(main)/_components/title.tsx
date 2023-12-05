a87cf81de50c532318b41488df05176d9bd59e43
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-05 11:11:56 +0600

    defining update endpoint mutation

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -1,5 +1,11 @@
 'use client';
 
-export const Title = () => {
+import { Doc } from '@/convex/_generated/dataModel';
+
+interface TItleProps {
+   initialData: Doc<'documents'>;
+}
+
+export const Title = ({ initialData }: TItleProps) => {
    return <div>Title</div>;
 };
