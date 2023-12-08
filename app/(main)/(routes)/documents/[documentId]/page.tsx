a4294fa283f7e98a64930c8752b72f62a4a0c8fb
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-08 10:52:48 +0600

    defining icon picker component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { Toolbar } from '@/components/toolbar';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useQuery } from 'convex/react';
@@ -19,6 +22,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
    return (
       <div className='pb-40'>
+         <div className='h-[35vh]' />
          <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
             <Toolbar initialData={document} />
          </div>
