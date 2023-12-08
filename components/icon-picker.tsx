1e846bdcc31dbe6913871ac5f47506ed1a5dcced
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-08 22:17:58 +0600

    Updated Icon Picker

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -3,7 +3,11 @@
 import EmojiPicker, { Theme } from 'emoji-picker-react';
 import { useTheme } from 'next-themes';
 
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import {
+   Popover,
+   PopoverContent,
+   PopoverTrigger,
+} from '@/components/ui/popover';
 
 interface IconPickerProps {
    onChange: (icon: string) => void;
@@ -28,10 +32,10 @@ export const IconPicker = ({
 
    return (
       <Popover>
-         <PopoverTrigger>{children}</PopoverTrigger>
+         <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
          <PopoverContent className='p-0 w-full border-none shadow-none'>
             <EmojiPicker
-               height={30}
+               height={350}
                theme={theme}
                onEmojiClick={(data) => onChange(data.emoji)}
             />
