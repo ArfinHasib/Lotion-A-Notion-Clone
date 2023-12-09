fe705899f6b60d36efd695686dc4421898d2cbda
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-09 20:19:05 +0600

    Toolbar content complete

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import { Doc } from '@/convex/_generated/dataModel';
-import { IconPicker } from './icon-picker';
-import { Button } from './ui/button';
+import { ElementRef, useRef, useState } from 'react';
 import { ImageIcon, Smile, X } from 'lucide-react';
-import React, { ElementRef, useRef, useState } from 'react';
 import { useMutation } from 'convex/react';
+import TextareaAutosize from 'react-textarea-autosize';
+
+// import { useCoverImage } from "@/hooks/use-cover-image";
+import { Doc } from '@/convex/_generated/dataModel';
+import { Button } from '@/components/ui/button';
 import { api } from '@/convex/_generated/api';
+
+import { IconPicker } from './icon-picker';
 
 interface ToolbarProps {
    initialData: Doc<'documents'>;
@@ -19,6 +23,9 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
    const [value, setValue] = useState(initialData.title);
 
    const update = useMutation(api.documents.update);
+   const removeIcon = useMutation(api.documents.removeIcon);
+
+   //   const coverImage = useCoverImage();
 
    const enableInput = () => {
       if (preview) return;
@@ -32,7 +39,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 
    const disableInput = () => setIsEditing(false);
 
-   const onInput = () => {
+   const onInput = (value: string) => {
       setValue(value);
       update({
          id: initialData._id,
@@ -47,17 +54,30 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
       }
    };
 
+   const onIconSelect = (icon: string) => {
+      update({
+         id: initialData._id,
+         icon,
+      });
+   };
+
+   const onRemoveIcon = () => {
+      removeIcon({
+         id: initialData._id,
+      });
+   };
+
    return (
       <div className='pl-[54px] group relative'>
          {!!initialData.icon && !preview && (
             <div className='flex items-center gap-x-2 group/icon pt-6'>
-               <IconPicker onChange={() => {}}>
+               <IconPicker onChange={onIconSelect}>
                   <p className='text-6xl hover:opacity-75 transition'>
                      {initialData.icon}
                   </p>
                </IconPicker>
                <Button
-                  onClick={() => {}}
+                  onClick={onRemoveIcon}
                   className='rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs'
                   variant='outline'
                   size='icon'
@@ -71,7 +91,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
          )}
          <div className='opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4'>
             {!initialData.icon && !preview && (
-               <IconPicker asChild onChange={() => {}}>
+               <IconPicker asChild onChange={onIconSelect}>
                   <Button
                      className='text-muted-foreground text-xs'
                      variant='outline'
@@ -84,21 +104,33 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             )}
             {!initialData.coverImage && !preview && (
                <Button
-                  onClick={() => {}}
+                  // onClick={coverImage.onOpen}
                   className='text-muted-foreground text-xs'
                   variant='outline'
                   size='sm'
                >
                   <ImageIcon className='h-4 w-4 mr-2' />
-                  Add Cover
+                  Add cover
                </Button>
             )}
          </div>
          {isEditing && !preview ? (
-
-         ): (
-
-         ) }
+            <TextareaAutosize
+               ref={inputRef}
+               onBlur={disableInput}
+               onKeyDown={onKeyDown}
+               value={value}
+               onChange={(e) => onInput(e.target.value)}
+               className='text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none'
+            />
+         ) : (
+            <div
+               onClick={enableInput}
+               className='pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]'
+            >
+               {initialData.title}
+            </div>
+         )}
       </div>
    );
 };
