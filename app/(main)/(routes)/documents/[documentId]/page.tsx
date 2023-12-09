d9736a5a9c3cdc2dbf20fad559c68be19d7fd977
ArfinHasib <arfinhasib21375@gmail.com>
2023-12-10 00:13:15 +0600

    Error Handling and custom error page

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -7,6 +7,8 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { useMutation, useQuery } from 'convex/react';
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
 
 interface DocumentIdPageProps {
    params: {
@@ -15,6 +17,11 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+   const Editor = useMemo(
+      () => dynamic(() => import('@/components/editor'), { ssr: false }),
+      []
+   );
+
    const document = useQuery(api.documents.getById, {
       documentId: params.documentId,
    });
